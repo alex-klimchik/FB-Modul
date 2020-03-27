04d3d61c56e27c43ecb8802c762c2c8dead9b742
Your Name <you@example.com>
2020-03-27 13:30:12 +0200

    sidebarAlex

---
+++ blocks/sidebar.php
@@ -0,0 +1,72 @@
+<nav id="mySidenav" class="sidebar">
+            <div class="sidebar-content">
+                <div class="sidebar-brand"><a><i class="fab fa-connectdevelop"></i>FB-Lagoon</a><a href="javascript:void(0)" class="closebtn"><i class="fas fa-times"></i></a></div>
+                <!-- <div class="sidebar-user">
+                    <img src="img/avatar.png" class="img-fluid rounded-circle mb-2" alt="Linda Miller">
+                    <div class="font-weight-bold">Имя Фамилия</div>
+                    <small>Админ FB-3</small>
+                    <br>
+                    <p style="margin:0;font-size: 10px;"> <i class="fas fa-circle text-success"></i> Online</p>
+                </div> -->
+                <ul class="sidebar-nav">
+                    <li class="sidebar-header">Меню</li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-home"></i> </span>
+                            <span class="title">Главная</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-user"></i> </span>
+                            <span class="title">Аккаунты</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-users"></i></i> </span>
+                            <span class="title">Сотрудники</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-chart-bar"></i> </span>
+                            <span class="title">Пресеты</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-list-ul"></i> </span>
+                            <span class="title">Этапы</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-chart-line"></i> </span>
+                            <span class="title">Статистика</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-folder-open"></i> </span>
+                            <span class="title">Общая воронка</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-archive"></i></span>
+                            <span class="title">Вайты</span></a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class="fas fa-box"></i></span>
+                            <span class="title">Задачи</span>
+                            <span size="18" class="sidebar-badge badge-primary badge-pill">New</span></a>
+                    </li>
+
+
+                    <li class="sidebar-header">Расширение для админа</li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="index.php">
+                            <span class="icon-holder"><i class=" fas fa-cog"></i> </span>
+                            <span class="title">Настройки</span>
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </nav>
