78aae1457d511aad288fac3622331e9accd36ac5
Inna <inna@lagoon.me>
2020-04-13 12:51:34 +0200

    add new styles page settings

--- settings.php
+++ settings.php
@@ -19,6 +19,10 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
 
+    <!--feather-icons-->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
@@ -43,13 +47,18 @@
 
                     <div class="main-content">
                         <div class="card card-bloks-shadow content-box">
-                            <p>Антидетект</p>
+                            <div class="stages-headline access-bg">
+                                <h5>Антидетект</h5>
+                            </div>
+<!--                            <p>Антидетект</p>-->
                             <div class="form-group">
                                 <!-- <label for="ant">Добавить</label> -->
                                 <input type="text" class="form-control" id="ant" placeholder="Добавить">
                             </div>
-                            <ul>
-                                <li>MLA</li>
+                            <ul class="task-list">
+                                <li>
+                                    <i class="mr-2 task-ic" data-feather="minus"></i>MLA
+                                </li>
                             </ul>
                             <div class="btns">
                                 <!-- <button type="button" class="btn btn-danger">
@@ -61,15 +70,18 @@
                             </div>
                         </div>
                         <div class="card card-bloks-shadow content-box">
-                            <p>Тип регистрации</p>
+                            <div class="stages-headline access-bg">
+                                <h5>Тип регистрации</h5>
+                            </div>
+<!--                            <p>Тип регистрации</p>-->
                             <div class="form-group">
                                 <!-- <label for="registr">Добавить</label> -->
                                 <input type="text" class="form-control" id="registr" placeholder="Добавить">
                             </div>
-                            <ul>
-                                <li>E-mail</li>
-                                <li>Телефон</li>
-                                <li>MLA</li>
+                            <ul class="task-list">
+                                <li><i class="mr-2 task-ic" data-feather="minus"></i>E-mail</li>
+                                <li><i class="mr-2 task-ic" data-feather="minus"></i>Телефон</li>
+                                <li><i class="mr-2 task-ic" data-feather="minus"></i>MLA</li>
                             </ul>
                             <div class="btns">
                                 <!-- <button type="button" class="btn btn-danger">
@@ -81,14 +93,17 @@
                             </div>
                         </div>
                         <div class="card card-bloks-shadow content-box">
-                            <p>Тип прокси</p>
+                            <div class="stages-headline access-bg">
+                                <h5>Тип прокси</h5>
+                            </div>
+<!--                            <p>Тип прокси</p>-->
                             <div class="form-group">
                                 <!-- <label for="proxy">Добавить</label> -->
                                 <input type="text" class="form-control" id="proxy" placeholder="Добавить">
                             </div>
-                            <ul>
-                                <li>static</li>
-                                <li>3g/4g</li>
+                            <ul class="task-list">
+                                <li><i class="mr-2 task-ic" data-feather="minus"></i>static</li>
+                                <li><i class="mr-2 task-ic" data-feather="minus"></i>3g/4g</li>
                             </ul>
                             <div class="btns">
                                 <!-- <button type="button" class="btn btn-danger">
@@ -109,6 +124,10 @@
     </div>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/js-cookie@beta/dist/js.cookie.min.js"></script>
     <script src="js/script.js"></script>
+    <!--feather-icons-->
+    <script>
+        feather.replace()
+    </script>
 
 </body>
 
